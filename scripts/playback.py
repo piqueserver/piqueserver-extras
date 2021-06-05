a7cc938b195b0f07bf6802b83103ea9a02cb1320
Jipok <braaga@inbox.ru>
2021-06-05 23:03:14 +0500

    Add fixed version of aos_replay

---
+++ scripts/playback.py
@@ -0,0 +1,154 @@
+#!/usr/bin/env python3
+""" 
+Original script and description can be found here: 
+https://github.com/BR-/aos_replay
+This version contains quick fixes for newer versions of python and enet
+"""
+
+import sys
+
+FILE_VERSION = 1
+
+import argparse
+parser  = argparse.ArgumentParser(description="Playback some gameplay")
+parser.add_argument('file', default='replay.demo', help="File to read from")
+parser.add_argument('port', default=32887, type=int, help="The port to run on")
+args = parser.parse_args()
+
+import struct
+with open(args.file, "rb") as fh:
+	fmt = "BB"
+	fmtlen = struct.calcsize(fmt)
+	data = fh.read(fmtlen)
+	file_version, aos_version = struct.unpack(fmt, data)
+	if FILE_VERSION != file_version:
+		if FILE_VERSION < file_version:
+			print("This demo was recorded on a newer version of aos_replay.")
+		elif FILE_VERSION > file_version:
+			print("This demo was recorded on an older version of aos_replay.")
+		print("aos_replay version: %d" % FILE_VERSION)
+		print("Demo version: %d" % file_version)
+		sys.exit(1)
+
+class Client(object):
+	def __init__(self, peer, fh, start_time):
+		self.peer = peer
+		self.fh = fh
+		self.fh.read(struct.calcsize("BB"))
+		self.start_time = start_time
+		self.pause_time = 0
+		self.get_next_packet()
+		self.spawned = False
+		self.spam_time = None
+		self.playerinfo = [[0,0] for _ in range(32)]
+		self.playerid = None
+	def get_next_packet(self):
+		fmt = "fH"
+		fmtlen = struct.calcsize(fmt)
+		meta = self.fh.read(fmtlen)
+		if len(meta) < fmtlen:
+			raise EOFError("replay file finished")
+		self.timedelta, size = struct.unpack(fmt, meta)
+		self.data = self.fh.read(size)
+		if self.data[0] == 15: # state data
+			self.playerid = self.data[1]
+
+import enet
+from time import time
+host = enet.Host(enet.Address(bytes('0.0.0.0', 'utf-8'), args.port), 128, 1)
+host.compress_with_range_coder()
+clients = {}
+client_id = 0
+while True:
+	for cl in list(clients.values()):
+		if cl.pause_time > 0:
+			continue
+		if cl.spam_time is not None and cl.spam_time <= time():
+			pkt = struct.pack("bbb", 17, 35, 2) + str(cl.timedelta).encode('cp437', 'replace') #chat message
+			cl.peer.send(0, enet.Packet(pkt, enet.PACKET_FLAG_RELIABLE))
+			cl.spam_time = time() + 1
+		while cl.start_time + cl.timedelta <= time():
+			if cl.data[0] == 3: #input data
+				player, data = struct.unpack("xbb", cl.data)
+				cl.playerinfo[player][0] = data
+			elif cl.data[0] == 4: #weapon data
+				player, data = struct.unpack("xbb", cl.data)
+				cl.playerinfo[player][1] = data
+			cl.peer.send(0, enet.Packet(cl.data, enet.PACKET_FLAG_RELIABLE))
+			try:
+				cl.get_next_packet()
+			except EOFError:
+				print(cl.peer.data, "finished playback")
+				cl.peer.disconnect(0) #ERROR_UNDEFINED
+				cl.fh.close()
+				del clients[cl.peer.data]
+				break
+	try:
+		event = host.service(0)
+	except IOError:
+		continue
+	if event is None:
+		continue
+	elif event.type == enet.EVENT_TYPE_CONNECT:
+		if event.peer.eventData == aos_version:
+			event.peer.data = bytes(str(client_id), 'utf-8')
+			client_id += 1
+			clients[event.peer.data] = Client(event.peer, open(args.file, "rb"), time())
+			print("received client connection", event.peer.data)
+		else:
+			print("WRONG CLIENT VERSION: replay is version %s and client was version %s" % (aos_version, event.peer.eventData))
+			event.peer.disconnect_now(3) #ERROR_WRONG_VERSION
+	elif event.type == enet.EVENT_TYPE_DISCONNECT:
+		if event.peer.data in clients:
+			clients[event.peer.data].fh.close()
+			del clients[event.peer.data]
+		print("lost client connection", event.peer.data)
+	elif event.type == enet.EVENT_TYPE_RECEIVE:
+		cl = clients[event.peer.data]
+		if not cl.spawned and cl.playerid is not None:
+			cl.spawned = True
+			pkt = struct.pack("bbbbfff16s", 12, cl.playerid, 1, -1, 255., 255., 1., b"")
+			event.peer.send(0, enet.Packet(pkt, enet.PACKET_FLAG_RELIABLE))
+		elif event.packet.data[0] == 17:
+			chat = event.packet.data[3:-1].decode('cp437', 'replace')
+			if chat == "spawn":
+				if cl.playerid is None:
+					print("error: could not figure out player id, guessing! use the command 'id X' to fix")
+					cl.playerid = 0
+				cl.spawned = True
+				pkt = struct.pack("bbbbfff16s", 12, cl.playerid, 1, -1, 255., 255., 1., "asd") #create player
+				event.peer.send(0, enet.Packet(pkt, enet.PACKET_FLAG_RELIABLE))
+			elif chat[:3] == "id ":
+				try:
+					playerid = int(chat[3:])
+				except:
+					pass
+				else:
+					cl.playerid = playerid
+			elif chat == "pause" and cl.pause_time == 0:
+				cl.pause_time = time()
+				for i in range(32):
+					pkt = struct.pack("bbb", 3, i, 0) #input data
+					event.peer.send(0, enet.Packet(pkt, enet.PACKET_FLAG_RELIABLE))
+					pkt = struct.pack("bbb", 4, i, 0) #weapon data
+					event.peer.send(0, enet.Packet(pkt, enet.PACKET_FLAG_RELIABLE))
+			elif chat == "unpause" and cl.pause_time > 0:
+				cl.start_time += time() - cl.pause_time
+				cl.pause_time = 0
+				for i in range(32):
+					pkt = struct.pack("bbb", 3, i, cl.playerinfo[i][0]) #input data
+					event.peer.send(0, enet.Packet(pkt, enet.PACKET_FLAG_RELIABLE))
+					pkt = struct.pack("bbb", 4, i, cl.playerinfo[i][1]) #weapon data
+					event.peer.send(0, enet.Packet(pkt, enet.PACKET_FLAG_RELIABLE))
+			elif chat[:3] == "ff ":
+				try:
+					skip = int(chat[3:])
+				except:
+					pass
+				else:
+					cl.start_time = cl.start_time - skip
+			elif chat == "time":
+				if cl.spam_time is None:
+					cl.spam_time = 0
+				else:
+					cl.spam_time = None
---
+++ scripts/record.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+""" 
+Original script and description can be found here: 
+https://github.com/BR-/aos_replay
+This version contains quick fixes for newer versions of python and enet
+"""
+
+FILE_VERSION = 1
+
+import argparse
+parser  = argparse.ArgumentParser(description="Record some gameplay")
+parser.add_argument('ip', help="The server's IP")
+parser.add_argument('port', type=int, nargs='?', default=-1, help="The server's port (default: 32887)")
+parser.add_argument('file', help="File to save to")
+versiongroup = parser.add_mutually_exclusive_group()
+versiongroup.add_argument('-75', action='store_const', dest='version', const=3, help="Use if the server is 0.75 (default)")
+versiongroup.add_argument('-76', action='store_const', dest='version', const=4, help="Use if the server is 0.76")
+parser.set_defaults(version=3)
+args = parser.parse_args()
+
+do_aos_conversion = args.ip.startswith("aos://")
+if do_aos_conversion:
+	args.ip = args.ip[6:]
+
+if args.port == -1:
+	try:
+		ip, port = args.ip.rsplit(':', 1)
+		args.port = int(port)
+		args.ip = ip
+	except ValueError:
+		args.port = 32887
+
+if do_aos_conversion:
+	dec = int(args.ip)
+	ip = ""
+	for _ in range(4):
+		ip += str(dec % 256) + "."
+		dec //= 256
+	if dec != 0:
+		print("ERROR: AoS address not valid?")
+		import sys
+		sys.exit()
+	args.ip = ip[:-1]
+
+#TODO: fix race condition
+import os
+if os.path.exists(args.file):
+	i = 1
+	while os.path.exists(args.file + "-" + str(i)):
+		i += 1
+	args.file += "-" + str(i)
+
+import struct
+import enet
+from time import time
+con = enet.Host(None, 1, 1)
+con.compress_with_range_coder()
+print('Trying to connect to: ' + args.ip)
+peer = con.connect(enet.Address(bytes(args.ip, 'utf-8'), args.port), 1, args.version)
+with open(args.file, "wb") as fh:
+	fh.write(struct.pack('BB', FILE_VERSION, args.version))
+	while True:
+		try:
+			event = con.service(1000)
+		except IOError:
+			continue
+		if event is None:
+			continue
+		elif event.type == enet.EVENT_TYPE_CONNECT:
+			print('connected to server')
+			start_time = time()
+		elif event.type == enet.EVENT_TYPE_DISCONNECT:
+			try:
+				reason = ["generic error", "banned", "kicked", "wrong version", "server is full"][event.data]
+			except KeyError:
+				reason = "unknown reason (%s)" % event.data
+			print('lost connection to server:', reason)
+			break
+		elif event.type == enet.EVENT_TYPE_RECEIVE:
+			#print(hex(ord(event.packet.data[0])))
+			fh.write(struct.pack('fH', time() - start_time, len(event.packet.data)))
+			fh.write(event.packet.data)
